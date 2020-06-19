e9f8a3e8ca431d7e7c5ad310d7af143c07ccc70b
Miguel Yurivilca <miguel.yurivilca.16@gmail.com>
2020-06-19 02:14:33 -0500

    hard coded videos

--- pages/videos.vue
+++ pages/videos.vue
@@ -1,81 +1,324 @@
 <template>
-  <div>
-    <DxTreeList
-      id="employees"
-      ref="treeList"
-      :data-source="employees"
-      :show-row-lines="true"
-      :show-borders="true"
-      :column-auto-width="true"
-      :expanded-row-keys="expandedRowKeys"
-      :selected-row-keys.sync="selectedRowKeys"
-      key-expr="ID"
-      parent-id-expr="Head_ID"
-    >
-      <DxSelection :recursive="recursive" mode="multiple" />
-      <DxColumn data-field="Full_Name" />
-      <DxColumn data-field="Title" caption="Position" />
-      <DxColumn data-field="City" />
-      <DxColumn data-field="State" />
-      <DxColumn :width="120" data-field="Hire_Date" data-type="date" />
-    </DxTreeList>
-    <div class="options">
-      <div class="caption">Options</div>
-      <div class="option">
-        <DxCheckBox
-          v-model="recursive"
-          text="Recursive Selection"
-          @value-changed="onRecursiveChanged"
-        />
-      </div>
-    </div>
-  </div>
-</template>
-<script>
-  import { DxTreeList, DxSelection, DxColumn } from 'devextreme-vue/tree-list'
-  import { DxCheckBox } from 'devextreme-vue/check-box'
-  import { employees } from '../static/js/data'
+  <v-row justify="center">
+    <v-col md="10">
+      <v-expansion-panels popout>
+        <v-expansion-panel v-for="grado in grados" :key="grado.grado">
+          <v-expansion-panel-header>
+            {{ grado.grado }}
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-expansion-panels popout>
+              <v-expansion-panel
+                v-for="curso in grado.cursos"
+                :key="curso.nombre"
+              >
+                <v-expansion-panel-header>
+                  {{ curso.nombre }}
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <v-expansion-panels popout>
+                    <v-expansion-panel
+                      v-for="unidad in curso.unidades"
+                      :key="unidad.nombre"
+                    >
+                      <v-expansion-panel-header>
+                        {{ unidad.nombre }}
+                      </v-expansion-panel-header>
+                      <v-expansion-panel-content>
+                        <v-row>
+                          <v-card
+                            :loading="loading"
+                            class="mx-auto my-12"
+                            max-width="374"
+                          >
+                            <v-img
+                              height="250"
+                              src="https://content.gnoss.ws/imagenesEnlaces/ab/ab79/ab79ad08-e602-42a9-8c88-56e154b78300/ab79ad08-e602-42a9-8c88-56e154b78300.jpg?1"
+                            ></v-img>
 
+                            <v-card-title>{{ unidad.nombre }}</v-card-title>
+
+                            <v-card-text>
+                              <v-row align="center" class="mx-0">
+                                <v-rating
+                                  :value="4.5"
+                                  color="amber"
+                                  dense
+                                  half-increments
+                                  readonly
+                                  size="14"
+                                ></v-rating>
+
+                                <div class="grey--text ml-4">4.5 (413)</div>
+                              </v-row>
+
+                              <div>
+                                {{ unidad.description }}
+                              </div>
+                            </v-card-text>
+
+                            <v-divider class="mx-4"></v-divider>
+
+                            <v-card-actions>
+                              <v-btn
+                                color="deep-purple lighten-2"
+                                text
+                                @click="reserve"
+                              >
+                                Guardar
+                              </v-btn>
+                            </v-card-actions>
+                          </v-card>
+                          <v-card
+                            :loading="loading"
+                            class="mx-auto my-12"
+                            max-width="374"
+                          >
+                            <v-img
+                              height="250"
+                              src="https://content.gnoss.ws/imagenesEnlaces/ab/ab79/ab79ad08-e602-42a9-8c88-56e154b78300/ab79ad08-e602-42a9-8c88-56e154b78300.jpg?1"
+                            ></v-img>
+
+                            <v-card-title>{{ unidad.nombre }}</v-card-title>
+
+                            <v-card-text>
+                              <v-row align="center" class="mx-0">
+                                <v-rating
+                                  :value="4.5"
+                                  color="amber"
+                                  dense
+                                  half-increments
+                                  readonly
+                                  size="14"
+                                ></v-rating>
+
+                                <div class="grey--text ml-4">4.5 (413)</div>
+                              </v-row>
+
+                              <div>
+                                {{ unidad.description }}
+                              </div>
+                            </v-card-text>
+
+                            <v-divider class="mx-4"></v-divider>
+
+                            <v-card-actions>
+                              <v-btn
+                                color="deep-purple lighten-2"
+                                text
+                                @click="reserve"
+                              >
+                                Guardar
+                              </v-btn>
+                            </v-card-actions>
+                          </v-card>
+                          <v-card
+                            :loading="loading"
+                            class="mx-auto my-12"
+                            max-width="374"
+                          >
+                            <v-img
+                              height="250"
+                              src="https://content.gnoss.ws/imagenesEnlaces/ab/ab79/ab79ad08-e602-42a9-8c88-56e154b78300/ab79ad08-e602-42a9-8c88-56e154b78300.jpg?1"
+                            ></v-img>
+
+                            <v-card-title>{{ unidad.nombre }}</v-card-title>
+
+                            <v-card-text>
+                              <v-row align="center" class="mx-0">
+                                <v-rating
+                                  :value="3"
+                                  color="amber"
+                                  dense
+                                  half-increments
+                                  readonly
+                                  size="14"
+                                ></v-rating>
+
+                                <div class="grey--text ml-4">3 (1000000)</div>
+                              </v-row>
+
+                              <div>
+                                {{ unidad.description }}
+                              </div>
+                            </v-card-text>
+
+                            <v-divider class="mx-4"></v-divider>
+
+                            <v-card-actions>
+                              <v-btn
+                                color="deep-purple lighten-2"
+                                text
+                                @click="reserve"
+                              >
+                                Guardar
+                              </v-btn>
+                            </v-card-actions>
+                          </v-card>
+                        </v-row>
+                      </v-expansion-panel-content>
+                    </v-expansion-panel>
+                  </v-expansion-panels>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
   export default {
     auth: false,
-    components: {
-      DxTreeList,
-      DxSelection,
-      DxColumn,
-      DxCheckBox
-    },
     data() {
       return {
-        employees,
-        expandedRowKeys: [1, 2, 10],
-        selectedRowKeys: [],
-        recursive: false
+        grados: [
+          {
+            grado: '3',
+            cursos: [
+              {
+                nombre: 'matematicas',
+                unidades: [
+                  {
+                    nombre: 'funciones 1',
+                    description: 'description',
+                    videos: []
+                  },
+                  {
+                    nombre: 'funciones 2',
+                    description: 'description',
+                    videos: []
+                  },
+                  {
+                    nombre: 'funciones 3',
+                    description: 'description',
+                    videos: []
+                  }
+                ]
+              },
+              {
+                nombre: 'quimica',
+                unidades: [
+                  {
+                    nombre: 'moleculas 1',
+                    description: 'description',
+                    videos: []
+                  },
+                  {
+                    nombre: 'moleculas 2',
+                    description: 'description',
+                    videos: []
+                  },
+                  {
+                    nombre: 'moleculas 3',
+                    description: 'description',
+                    videos: []
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            grado: '4',
+            cursos: [
+              {
+                nombre: 'matematicas',
+                unidades: [
+                  {
+                    nombre: 'funciones 1',
+                    description: 'description',
+                    videos: []
+                  },
+                  {
+                    nombre: 'funciones 2',
+                    description: 'description',
+                    videos: []
+                  },
+                  {
+                    nombre: 'funciones 3',
+                    description: 'description',
+                    videos: []
+                  }
+                ]
+              },
+              {
+                nombre: 'quimica',
+                unidades: [
+                  {
+                    nombre: 'moleculas 1',
+                    description: 'description',
+                    videos: []
+                  },
+                  {
+                    nombre: 'moleculas 2',
+                    description: 'description',
+                    videos: []
+                  },
+                  {
+                    nombre: 'moleculas 3',
+                    description: 'description',
+                    videos: []
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            grado: '5',
+            cursos: [
+              {
+                nombre: 'matematicas',
+                unidades: [
+                  {
+                    nombre: 'funciones 1',
+                    description: 'description',
+                    videos: []
+                  },
+                  {
+                    nombre: 'funciones 2',
+                    description: 'description',
+                    videos: []
+                  },
+                  {
+                    nombre: 'funciones 3',
+                    description: 'description',
+                    videos: []
+                  }
+                ]
+              },
+              {
+                nombre: 'quimica',
+                unidades: [
+                  {
+                    nombre: 'moleculas 1',
+                    description: 'description',
+                    videos: []
+                  },
+                  {
+                    nombre: 'moleculas 2',
+                    description: 'description',
+                    videos: []
+                  },
+                  {
+                    nombre: 'moleculas 3',
+                    description: 'description',
+                    videos: []
+                  }
+                ]
+              }
+            ]
+          }
+        ]
       }
     },
     methods: {
-      onRecursiveChanged() {
-        this.selectedRowKeys = []
+      async onSubmit() {
+        await this.$axios.$get()
       }
     }
   }
 </script>
-<style scoped>
-  #employees {
-    max-height: 440px;
-  }
 
-  .options {
-    padding: 20px;
-    margin-top: 20px;
-    background-color: rgba(191, 191, 191, 0.15);
-  }
-
-  .caption {
-    font-size: 18px;
-    font-weight: 500;
-  }
-
-  .option {
-    margin-top: 10px;
-  }
-</style>
+<style></style>
