d61f20fbbe347af7b413703d5bb056a4eb5bfa6e
Flrotm <francesco.uccelli@utec.edu.pe>
2020-06-24 01:36:31 -0500

    primer test

---
+++ pages/video.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <section>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/4JV3bmgGH6M"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Video'
+  }
+</script>
+
+<style scoped></style>
