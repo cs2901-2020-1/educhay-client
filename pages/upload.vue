201329d7ad260ae710729a317c897e9a88ab060f
luisejv <ljauregui@pucp.pe>
2020-07-07 00:57:39 -0500

    modeling upload videos

--- pages/upload.vue
+++ pages/upload.vue
@@ -7,24 +7,24 @@
     <button @click="execute()">execute</button> -->
       <v-col xs="12" sm="12" md="6">
         <v-row justify="center" align="center">
-          <v-btn color="rgb(25, 84, 140)" large fab dark>
-            <v-icon>mdi-television</v-icon>
-          </v-btn>
-        </v-row>
-      </v-col>
-      <v-col xs="12" sm="12" md="6">
-        <v-row justify="center" align="center">
-          <v-dialog v-model="showModal" persistent max-width="600px">
+          <v-dialog v-model="showUpload" persistent max-width="600px">
             <template v-slot:activator="{ on, attrs }">
-              <v-btn color="error" dark v-bind="attrs" v-on="on">
-                youtube
+              <v-btn
+                color="rgb(25, 84, 140)"
+                large
+                fab
+                dark
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon>mdi-television</v-icon>
               </v-btn>
             </template>
             <v-card>
               <v-card-title>
-                <span class="headline">Formulario</span>
+                <span class="headline">Subir video local</span>
                 <v-spacer />
-                <v-icon @click="showModal = false">mdi-window-close</v-icon>
+                <v-icon @click="showUpload = false">mdi-window-close</v-icon>
               </v-card-title>
               <v-card-text>
                 <v-container>
@@ -33,11 +33,6 @@
                       <v-text-field
                         v-model="form.title"
                         label="Título"
-                        required
-                      ></v-text-field>
-                      <v-text-field
-                        v-model="form.link"
-                        label="Link"
                         required
                       ></v-text-field>
                       <v-select
@@ -50,7 +45,7 @@
                       ></v-select>
                       <v-select
                         v-model="form.curso"
-                        :items="cursos"
+                        :items="cursosSelect"
                         :rules="[(v) => !!v || 'Debe seleccionar Curso']"
                         label="Curso"
                         required
@@ -58,34 +53,162 @@
                       ></v-select>
                       <v-select
                         v-model="form.unidad"
-                        :items="unidades"
+                        :items="unidadesSelect"
+                        item-text="nombre"
                         :rules="[(v) => !!v || 'Debe seleccionar Unidad']"
                         label="Unidad"
                         required
                       ></v-select>
+                      <v-file-input
+                        ref="file"
+                        id="file"
+                        :rules="rules"
+                        accept="video/*"
+                        v-model="files"
+                        color="blue accent-4"
+                        counter
+                        placeholder="Elegir video"
+                        prepend-icon="mdi-video"
+                        :show-size="1000"
+                        @change="handleFileUpload"
+                      >
+                        <template v-slot:selection="{ index, text }">
+                          <v-chip
+                            v-if="index < 2"
+                            color="blue accent-4"
+                            dark
+                            label
+                            small
+                          >
+                            {{ text }}
+                          </v-chip>
+
+                          <span
+                            v-else-if="index === 2"
+                            class="overline grey--text text--darken-3 mx-2"
+                          >
+                            +{{ files.length - 2 }} File(s)
+                          </span>
+                        </template>
+                      </v-file-input>
                       <v-card-text>
-                        <v-row>
-                          <v-col class="pr-4">
+                        <v-row justify="center" align="center">
+                          <v-col md="6">
                             <v-subheader class="pl-0">Rating</v-subheader>
-                            <v-slider
+                            <v-rating
                               v-model="form.rating"
-                              class="align-center"
-                              :max="slide.max"
-                              :min="slide.min"
-                              thumb-label="always"
-                            >
-                            </v-slider>
+                              color="yellow darken-3"
+                              background-color="grey darken-1"
+                              empty-icon="$ratingFull"
+                              half-increments
+                              hover
+                            ></v-rating>
                           </v-col>
                         </v-row>
                       </v-card-text>
                     </v-col>
                   </v-row>
                   <v-row>
-                    <v-col>
-                      <v-btn @click="checkId">
-                        Check
-                      </v-btn>
+                    <v-col offset-md="8">
+                      <v-card-actions>
+                        <v-spacer />
+                        <v-btn color="primary" @click="submitFile"
+                          >Enviar</v-btn
+                        >
+                      </v-card-actions>
                     </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+            </v-card>
+          </v-dialog>
+        </v-row>
+      </v-col>
+      <v-col xs="12" sm="12" md="6">
+        <v-row justify="center" align="center">
+          <v-dialog v-model="showModal" persistent max-width="600px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="error" dark v-bind="attrs" v-on="on">
+                youtube
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">Ingresar video de YouTube</span>
+                <v-spacer />
+                <v-icon @click="showModal = false">mdi-window-close</v-icon>
+              </v-card-title>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col>
+                      <v-row>
+                        <v-text-field
+                          v-model="form.title"
+                          label="Título"
+                          required
+                        ></v-text-field>
+                      </v-row>
+                      <v-row>
+                        <v-text-field
+                          v-model="form.link"
+                          label="Link"
+                          required
+                        ></v-text-field>
+                        <v-btn @click="checkId">
+                          Check
+                        </v-btn>
+                      </v-row>
+                      <v-row>
+                        <v-select
+                          v-model="form.grado"
+                          :items="grados"
+                          :rules="[(v) => !!v || 'Debe seleccionar Grado']"
+                          label="Grado"
+                          required
+                          @input="cursosPorGrado"
+                        ></v-select>
+                      </v-row>
+                      <v-row>
+                        <v-select
+                          v-model="form.curso"
+                          :items="cursosSelect"
+                          :rules="[(v) => !!v || 'Debe seleccionar Curso']"
+                          label="Curso"
+                          required
+                          @input="unidadesPorCurso"
+                        ></v-select>
+                      </v-row>
+                      <v-row>
+                        <v-select
+                          v-model="form.unidad"
+                          :items="unidadesSelect"
+                          item-text="nombre"
+                          :rules="[(v) => !!v || 'Debe seleccionar Unidad']"
+                          label="Unidad"
+                          required
+                        ></v-select>
+                      </v-row>
+                      <v-row>
+                        <v-card-text>
+                          <v-row justify="center" align="center">
+                            <v-col md="6">
+                              <v-subheader class="pl-0">Rating</v-subheader>
+                              <v-rating
+                                v-model="form.rating"
+                                color="yellow darken-3"
+                                background-color="grey darken-1"
+                                empty-icon="$ratingFull"
+                                half-increments
+                                hover
+                              ></v-rating>
+                            </v-col>
+                          </v-row>
+                        </v-card-text>
+                      </v-row>
+                    </v-col>
+                  </v-row>
+                  <v-row>
                     <v-col v-if="message">
                       <v-alert v-if="message === 'success'" type="success">
                         Success
@@ -94,15 +217,17 @@
                         Error
                       </v-alert>
                     </v-col>
+                    <v-col offset-md="8">
+                      <v-card-actions>
+                        <v-spacer />
+                        <v-btn color="primary" @click="uploadVideo"
+                          >Enviar</v-btn
+                        >
+                      </v-card-actions>
+                    </v-col>
                   </v-row>
                 </v-container>
               </v-card-text>
-              <v-card-actions>
-                <v-spacer />
-                <v-btn color="blue darken-1" text @click="uploadVideo"
-                  >Enviar</v-btn
-                >
-              </v-card-actions>
             </v-card>
           </v-dialog>
         </v-row>
@@ -126,6 +251,7 @@
           range: [0, 5]
         },
         showModal: false,
+        showUpload: false,
         form: {
           grado: '',
           unidad: '',
@@ -137,10 +263,20 @@
         apiKey: 'AIzaSyAMZy4Hg0tcdVkisz6o_BVwJLFWyuM4V_I',
         message: null,
         title: '',
+        cursosSelect: [],
+        unidadesSelect: [],
         grados: [],
-        cursos: null,
-        unidades: null,
-        datos: {}
+        cursos: [],
+        unidades: [],
+        datos: {},
+        rules: [
+          (value) =>
+            !value ||
+            value.size < 128000000 ||
+            'El video debe pesar maximo 128 MB!'
+        ],
+        file: '',
+        urlUpload: ''
       }
     },
     computed: {
@@ -157,16 +293,60 @@
       }) */
     },
     methods: {
+      submitFile() {
+        const formData = new FormData()
+        formData.append('file', this.file)
+        this.$axios
+          .post('/uploadFile', formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          })
+          .then((res) => {
+            this.urlUpload = res
+            this.uploadLocalVideo()
+          })
+          .catch(function() {
+            console.log('gee')
+          })
+      },
+      handleFileUpload() {
+        this.file = this.$refs.file.files[0]
+      },
       cursosPorGrado() {
-        if (this.form.grado !== '') {
-          this.cursos = this.datos[this.form.grado]
-          console.log(this.cursos)
+        let pos = 0
+        for (let i = 0; i < this.grados.length; i++) {
+          if (this.grados[i] === this.form.grado) {
+            pos = i
+          }
         }
+        this.cursosSelect = Object.keys(this.cursos[pos])
       },
       unidadesPorCurso() {
-        if (this.form.curso !== '') {
-          this.unidades = this.datos[this.form.grado][this.form.curso]
+        let pos = 0
+        for (let i = 0; i < this.grados.length; i++) {
+          if (this.grados[i] === this.form.grado) {
+            pos = i
+          }
         }
+        this.unidadesSelect = this.cursos[pos][this.form.curso]
+      },
+      async uploadLocalVideo() {
+        const url = '/videos/POST'
+        await this.$axios
+          .$post(url, {
+            creador_email: 'profe@utec.edu.pe',
+            _unidad: this.form.unidad.nombre,
+            url_stream: this.urlUpload,
+            titulo: this.form.title,
+            url_download: ''
+          })
+          .then((res) => {
+            console.log('correcto')
+          })
+          .catch((e) => {
+            console.log('error')
+          })
       },
       async uploadVideo() {
         const url = '/videos/POST'
@@ -210,24 +390,12 @@
       },
       async onSubmit() {
         const url = '/unidades'
-        // console.log('onsubmit')
         await this.$axios
           .$get(url)
           .then((response) => {
-            console.log(response)
             console.log('onSubmit')
-            // this.datos = response
-            const dataMap = new Map()
             this.grados = Object.keys(response)
-            // this.cursos = Object.values(Object.values(response))
-            for (let i = 0; i < this.grados.length; i++) {
-              dataMap.set(this.grados[i], this.cursos[i])
-            }
-            this.datos = dataMap
-            console.log(dataMap)
-            // console.log(this.grados)
-            // console.log(this.cursos)
-            // console.log(this.unidades)
+            this.cursos = Object.values(response)
           })
           .catch((e) => {
             console.log(e)
